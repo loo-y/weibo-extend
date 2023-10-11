f73987b416d1f65a6394f3484f8138fcdf3ec520
Loo <loo@Mac-mini-M2.lan>
2023-10-11 21:00:23 +0800

    chore: add bun build

---
+++ bunpack.ts
@@ -0,0 +1,51 @@
+const { exec } = require('child_process')
+import path from 'path'
+
+const removeFile = (filePath: string, isForce?: boolean) => {
+    if (!filePath) return
+    return new Promise((resolve, reject) => {
+        const command = `rm ${isForce ? '-rf' : ''} ${filePath}`
+
+        try {
+            // @ts-ignore
+            exec(command, (err, stdout, stderr) => {
+                if (err) {
+                    console.error('Error removing file:', err)
+                    resolve(false)
+                } else {
+                    console.log('File removed successfully', filePath)
+                    resolve(true)
+                }
+            })
+        } catch (e) {
+            console.log('Error removing file catch:', e)
+            resolve(false)
+        }
+    })
+}
+
+async function bunBuild() {
+    const outDir = path.resolve(__dirname, './extension')
+    const contentScript = path.resolve(__dirname, './app/scripts/content/')
+    const builtContentScript = path.resolve(outDir, './content-script.js')
+
+    const injectScript = path.resolve(__dirname, './app/scripts/inject/')
+    const builtInjectScript = path.resolve(outDir, './inject-script.js')
+
+    // 临时移除 removeFile 调用， bun的bug。 TODO
+    // await Promise.all([
+    //     removeFile(builtContentScript, true),
+    //     removeFile(builtInjectScript, true),
+    // ])
+
+    // @ts-ignore
+    await Bun.build({
+        entrypoints: [contentScript, injectScript],
+        target: 'browser',
+        minify: true,
+        outdir: outDir,
+        naming: '[dir]-script.[ext]',
+    })
+}
+
+bunBuild()
